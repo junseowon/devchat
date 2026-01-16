e804731a5a483f756385b4b52e7cd5bba2ab2fa6
junseowon <junseowon@gmail.com>
2026-01-16 17:44:51 +0900

    chore: Setup Spring Boot project with minimal dependencies

---
+++ src/main/java/io/github/junseowon/devchat/DevchatApplication.java
@@ -0,0 +1,13 @@
+package io.github.junseowon.devchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/junseowon/devchat/DevchatApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.junseowon.devchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
